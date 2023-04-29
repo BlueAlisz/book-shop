a42ea81542690dd82ed1600f3688e645bf5581a2
Nattaphat Yadfung <nattaphatblue.2545@gmail.com>
2023-04-30 02:27:59 +0700

    update: adding content to recipes component

--- book-shop/src/app/recipes/recipe-list/recipe-list.component.ts
+++ book-shop/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'egg',
+      'egg from chicken',
+      'https://static01.nyt.com/images/2019/02/05/world/05egg/15xp-egg-promo-superJumbo-v2.jpg'
+    ),
+  ];
 
   constructor() {}
 
