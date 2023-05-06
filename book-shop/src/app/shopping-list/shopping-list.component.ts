56391a8eb392699cf8f7dd62068dc0d50629a62e
Nattaphat Yadfung <nattaphatblue.2545@gmail.com>
2023-05-07 00:00:57 +0700

    update: adding shopping list service

--- book-shop/src/app/shopping-list/shopping-list.component.ts
+++ book-shop/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredients.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,16 +8,11 @@ import { Ingredient } from '../shared/ingredients.model';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('apple', 5),
-    new Ingredient('tomatoes', 5),
-  ];
+  ingredients: Ingredient[];
 
-  constructor() {}
+  constructor(private slService: ShoppingListService) {}
 
-  ngOnInit(): void {}
-
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+  ngOnInit() {
+    this.ingredients = this.slService.getIngredients();
   }
 }
--- book-shop/src/app/shopping-list/shopping-list.service.ts
+++ book-shop/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
+import { Ingredient } from '../shared/ingredients.model';
+
 export class ShoppingListService {
-    
+  private ingredients: Ingredient[] = [
+    new Ingredient('apple', 5),
+    new Ingredient('tomatoes', 5),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredients(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
