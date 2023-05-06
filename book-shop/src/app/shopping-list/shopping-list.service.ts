9ec478d9c41de0bda1bb28630fca331cfb6ba303
Nattaphat Yadfung <nattaphatblue.2545@gmail.com>
2023-05-07 00:12:24 +0700

    update: Using Services for Pushing Data from A to B

--- book-shop/src/app/shopping-list/shopping-list.service.ts
+++ book-shop/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredients.model';
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('apple', 5),
     new Ingredient('tomatoes', 5),
@@ -12,5 +14,6 @@ export class ShoppingListService {
 
   addIngredients(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
